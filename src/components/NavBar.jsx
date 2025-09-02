25f87c972d12534864dc165b9725ce51fc3824dd
TharunvenkateshN <tharunnv98@gmail.com>
2025-09-02 17:01:09 +0530

    Initial commit - CineMate React website

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,23 +1,51 @@
-import { Link } from "react-router-dom"
+import { useState } from "react";
+import { Link } from "react-router-dom";
 import "../css/Navbar.css";
 
-
 function NavBar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <div className="app">
-    <div className="navblock">
-      <nav className="navbar">
-        <div className="navbar-brand">
-          <Link to="/"><img src="/cinelogo.png" alt="Cinemate Logo" className="logo" /></Link>
-        </div>
-        <div className="navbar-links">
-          <Link to="/" className="nav-link">Home</Link>
-          <Link to="/favourites" className="nav-link">Favourites</Link>
-        </div>
-      </nav>
+      <div className="navblock">
+        <nav className="navbar">
+          <div className="navbar-brand">
+            <Link to="/">
+              <img src="/cinelogo.png" alt="Cinemate Logo" className="logo" />
+            </Link>
+          </div>
+
+          {/* Hamburger button */}
+          <div
+            className={`hamburger ${menuOpen ? "active" : ""}`}
+            onClick={toggleMenu}
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+
+          {/* Links */}
+          <div className={`navbar-links ${menuOpen ? "active" : ""}`}>
+            <Link to="/" className="nav-link" onClick={() => setMenuOpen(false)}>
+              Home
+            </Link>
+            <Link
+              to="/favourites"
+              className="nav-link"
+              onClick={() => setMenuOpen(false)}
+            >
+              Favourites
+            </Link>
+          </div>
+        </nav>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
